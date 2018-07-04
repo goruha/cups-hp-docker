253fb97d7c915478ded2ffd77b155bc6c27490cf
Igor Rodionov <goruha@gmail.com>
2018-07-04 13:35:42 +0600

    Fix Password

--- rootfs/start.sh
+++ rootfs/start.sh
@@ -8,4 +8,4 @@ if [ $(grep -ci $CUPS_USER_ADMIN /etc/shadow) -eq 0 ]; then
     useradd $CUPS_USER_ADMIN --system -G root,lpadmin --no-create-home --password $CUPS_USER_PASSWORD
 fi
 
-exec /usr/sbin/cupsd -f
+/usr/sbin/cupsd -f
